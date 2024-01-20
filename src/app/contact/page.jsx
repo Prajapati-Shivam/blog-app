43c772f6176adfef11b512a1f69e850fdd8f530c
Shivam <prajapatishivam11203@gmail.com>
2024-01-20 23:05:20 +0530

    Add contact form and image to ContactPage

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,48 @@
+import Image from "next/image";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className="flex items-center gap-10">
+      <div className="flex-1 relative h-[500px]">
+        <Image
+          src="/contact.png"
+          alt="contact image"
+          fill
+          className="object-contain"
+        />
+      </div>
+      <div className="flex-1 flex">
+        <form action="" className="flex-1 flex flex-col gap-5">
+          <input
+            type="text"
+            placeholder="Your name"
+            className="p-4 rounded border-none outline-none bg-[#2d2b42]"
+          />
+          <input
+            type="email"
+            placeholder="Email Address"
+            className="p-4 rounded border-none outline-none bg-[#2d2b42]"
+          />
+          <input
+            type="number"
+            placeholder="Phone Number (Optional)"
+            className="p-4 rounded border-none outline-none bg-[#2d2b42]"
+          />
+          <textarea
+            name="message"
+            id=""
+            cols="30"
+            rows="5"
+            placeholder="Message"
+            className="p-4 rounded border-none outline-none bg-[#2d2b42]"
+          ></textarea>
+          <button className="px-4 py-3 bg-[#367dfd] text-white cursor-pointer font-bold border-none outline-none rounded">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
