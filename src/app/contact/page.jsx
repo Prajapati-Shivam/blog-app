d5072dedb12630f4f57fffda0352aa25b32fc8b0
Shivam <prajapatishivam11203@gmail.com>
2024-03-11 22:11:29 +0530

    Update contact page layout and input styles

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const ContactPage = () => {
   return (
     <div className="flex items-center gap-10">
-      <div className="flex-1 hidden md:flex relative h-[500px]">
+      <div className="flex-1 hidden md:flex relative h-[450px]">
         <Image
           src="/contact.png"
           alt="contact image"
@@ -11,30 +11,30 @@ const ContactPage = () => {
           className="object-contain"
         />
       </div>
-      <div className="flex-1 flex">
+      <div className="flex-1 flex my-6 sm:my-0">
         <form action="" className="flex-1 flex flex-col gap-5">
           <input
             type="text"
             placeholder="Your name"
-            className="p-4 rounded border-none outline-none bg-[#2d2b42]"
+            className="p-2 rounded border-none outline-none bg-[#2d2b42]"
           />
           <input
             type="email"
             placeholder="Email Address"
-            className="p-4 rounded border-none outline-none bg-[#2d2b42]"
+            className="p-2 rounded border-none outline-none bg-[#2d2b42]"
           />
           <input
             type="number"
             placeholder="Phone Number (Optional)"
-            className="p-4 rounded border-none outline-none bg-[#2d2b42]"
+            className="p-2 rounded border-none outline-none bg-[#2d2b42]"
           />
           <textarea
             name="message"
             id=""
             cols="30"
-            rows="5"
+            rows="4"
             placeholder="Message"
-            className="p-4 rounded border-none outline-none bg-[#2d2b42]"
+            className="p-2 rounded border-none outline-none bg-[#2d2b42]"
           ></textarea>
           <button className="px-4 py-3 bg-[#367dfd] text-white cursor-pointer font-bold border-none outline-none rounded">
             Send Message
